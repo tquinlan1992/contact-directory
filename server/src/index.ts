5e1b66731c317e61d114bde8cfaef35af753ab56
Tom Quinlan <tquinlan1992@gmail.com>
2021-07-04 10:10:11 -0400

    set up root npm start that builds client code then starts server

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,11 +2,7 @@ import express from "express";
 const app = express();
 const port = 8080; // default port to listen
 
-// define a route handler for the default home page
-app.get("/", (req, res) => {
-  // render the index template
-  res.json("Hello world");
-});
+app.use(express.static(__dirname + "/../../client/build"));
 
 // start the express server
 app.listen(port, () => {
